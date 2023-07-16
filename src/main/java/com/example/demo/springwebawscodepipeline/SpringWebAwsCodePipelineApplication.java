1eb2c22d3d65fb143f6c0872ac1f3623d1680938
Mallikarjuna Vangiwaka <mallilavanya2010@gmail.com>
2023-07-16 11:52:13 -0400

    first commit

---
+++ src/main/java/com/example/demo/springwebawscodepipeline/SpringWebAwsCodePipelineApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo.springwebawscodepipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringWebAwsCodePipelineApplication {
+	
+	@GetMapping("/")
+	public String getMessage() {
+		return "AWS codepipeline example successfully running with tech stack springboot --> github --> aws code pipeline --> aws code build --> Elastic bean stack";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebAwsCodePipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/springwebawscodepipeline/SpringWebAwsCodePipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.springwebawscodepipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebAwsCodePipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
